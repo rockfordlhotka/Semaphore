a4eff97c2d4f3718b449642c003984d1defb068f
Rockford Lhotka <rocky@lhotka.net>
2016-08-07 21:10:17 -0700

    Initial add of code from old svn repo

---
+++ Semaphore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Semaphore.Max = 3;
+      for (int i = 0; i < 50; i++)
+      {
+        System.Threading.ThreadPool.QueueUserWorkItem(DoWork, i);
+        //var t = new System.Threading.Thread((o) =>
+        //  {
+        //    using (new Semaphore())
+        //    {
+        //      System.Threading.Thread.Sleep(10);
+        //      Console.WriteLine(o);
+        //    }
+        //  });
+        //t.Start(i);
+      }
+
+      Console.ReadLine();
+    }
+
+    private static void DoWork(object o)
+    {
+      using (new Semaphore())
+      {
+        System.Threading.Thread.Sleep(100);
+        Console.WriteLine(o);
+      }
+    }
+  }
+}
---
+++ Semaphore/Semaphore.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+  public class Semaphore : IDisposable
+  {
+    private static object _lock = new object();
+    private static int _active;
+    private static int _max = 1;
+
+    public static int Max
+    {
+      get { return _max; }
+      set 
+      { 
+        lock (_lock)
+          _max = value; 
+      }
+    }
+
+    public static int Active
+    {
+      get { return _active; }
+    }
+
+    public Semaphore()
+    {
+      bool okToRun = false;
+      do
+      {
+        if (_active < _max)
+        {
+          lock (_lock)
+          {
+            if (_active < _max)
+            {
+              _active++;
+              Console.WriteLine("+Active " + _active.ToString());
+              okToRun = true;
+            }
+          }
+        }
+        else
+        {
+          Console.WriteLine("=Blocked " + _active.ToString());
+          Thread.Sleep(2);
+        }
+      } while (!okToRun);
+    }
+
+    public void Dispose()
+    {
+      lock (_lock)
+      {
+        Console.WriteLine("-Active " + _active.ToString());
+        _active--;
+      }
+    }
+  }
+}
